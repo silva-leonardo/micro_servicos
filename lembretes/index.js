5169c9c5bcca47a7aa0b80dd6d0b2c568fbe5f46
asbonato <asbonato@gmail.com>
2022-09-13 21:50:00 -0300

    apostila 03 de microsserviços, finalizada a seção 4.3.34, até o bloco de código 4.3.15

--- lembretes/index.js
+++ lembretes/index.js
@@ -1,7 +1,9 @@
 const express = require ('express')
 const app = express()
+const axios = require('axios')
 //estamos aplicando um middleware
 app.use(express.json())
+
 
 const lembretes = {}
 let contador = 0
@@ -15,12 +17,25 @@ app.get('/lembretes', (req, res) => {
 //POST cadastrar um lembrete novo
 //localhost:4000/lembretes
 // {texto: 'Fazer café'}
-app.post('/lembretes', (req, res) => {
+app.post('/lembretes', async (req, res) => {
   contador++
   // const texto = req.body.texto  
-  const { texto } = req.body
-  lembretes[contador] = {contador, texto}
-  res.status(201).end()
+  const { texto } = req.body;
+  lembretes[contador] = {contador, texto};
+  await axios.post("http://localhost:10000/eventos", {
+    tipo: "Lembrete Criado",
+    dados: {
+      contador,
+      texto,
+    },
+  });
+
+  res.status(201).send(lembretes[contador]);
+})
+
+app.post('/eventos', (req, res) => {
+  console.log(req.body);
+  res.status(200).send({msg: 'ok'});
 })
 
 app.listen(4000, () => console.log ('Lembretes. Porta 4000'))
--- observacoes/index.js
+++ observacoes/index.js
@@ -1,18 +1,43 @@
 const express = require ('express')
 const app = express()
+const axios = require('axios');
 //erro comum: esquecer de chamar a função
 //json 
 //não faça isso: app.use(express.json)
 app.use(express.json())
 
-//localhost:5000/lembretes/123456/observacoes
-app.post('/lembretes/:id/observacoes', (req, res) => {
+const {v4 : uuidv4} = require('uuid');
 
+const observacoesPorLembreteId = {};
+
+//localhost:5000/lembretes/123456/observacoes
+app.post('/lembretes/:id/observacoes', async (req, res) => {
+    const idObs = uuidv4();
+    const { texto } = req.body;
+    //req.params dá acesso à lista de parâmetros da URL
+    const observacoesDoLembrete = 
+        observacoesPorLembreteId[req.params.id] || [];
+    observacoesDoLembrete.push({id: idObs, texto});
+    observacoesPorLembreteId[req.params.id] = observacoesDoLembrete;
+    await axios.post('http://localhost:10000/eventos', {
+        tipo: 'ObservacaoCriada',
+        dados: {
+            id: idObs,
+            texto,
+            lembreteId: req.params.id
+        }
+    })
+    res.status(201).send(observacoesDoLembrete);
 })
 
 //localhost:5000/lembretes/abcd/observacoes
 app.get('/lembretes/:id/observacoes', (req, res) => {
-
+    res.send(observacoesPorLembreteId[req.params.id] || []);
 })
 
+app.post('/eventos', (req, res) => {
+    console.log(req.body);
+    res.status(200).send({msg: 'ok'});
+  })
+  
 app.listen(5000, () => console.log('Observações. Porta 5000.'))
